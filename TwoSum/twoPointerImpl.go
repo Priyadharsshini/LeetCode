893fd7e739290d97f363a4ea544fd5d5e6e22e48
Priyadharsshini <dharsshinisakrapani@gmail.com>
2023-10-23 06:02:20 -0230

    Implementation using two pointer

---
+++ TwoSum/twoPointerImpl.go
@@ -0,0 +1,14 @@
+// This solution does not give the index og the original array, as the array is now sorted inplace.
+func twoSum(nums []int, target int) []int {
+    sort.Ints(nums)
+    for i,j := 0,  len(nums) - 1; i < len(nums) && j >= i;  {
+        if nums[j] + nums[i] == target {
+            return []int{nums[j],nums[i]}   
+        } else if nums[j] + nums[i] > target {
+            j--
+        } else {
+            i++
+        }
+    }
+    return []int{-1,-1} 
+}
