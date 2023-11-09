4fc4fd954ac404eb96cbc83f9da932157e53a871
Priyadharsshini <dharsshinisakrapani@gmail.com>
2023-11-09 08:14:10 -0330

    Create inPlace.go

---
+++ RotateArray/inPlace.go
@@ -0,0 +1,23 @@
+func rotate(nums []int, k int)  {
+    // First rotate the whole array
+    start, end := 0, len(nums)-1
+    for start < end {
+        nums[start], nums[end] = nums[end], nums[start]
+        start++
+        end--
+    }
+    //Rotate the first k elements 
+    start, end = 0, k-1
+    for start < end {
+        nums[start], nums[end] = nums[end], nums[start]
+        start++
+        end--
+    }
+    //Rotate the last k elements 
+    start,end = k, len(nums)-1
+    for start < end {
+        nums[start], nums[end] = nums[end], nums[start]
+        start++
+        end--
+    }
+}
